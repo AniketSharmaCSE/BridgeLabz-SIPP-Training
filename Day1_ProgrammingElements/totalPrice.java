a3b3f315d1cd69792d5165487a4e0fcdde6c2197
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-06-25 11:24:28 +0530

    [Aniket] Fix Cleaning OOPS branch

--- Day1_ProgrammingElements/totalPrice.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class totalPrice{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		double unitprice = sc.nextDouble(), quantity = sc.nextDouble();
-		double totalPrice = unitprice*quantity;
-		System.out.println("The total purchase price is INR "+totalPrice+" if the quantity "+quantity+ "and unit price is INR "+unitprice);
-	}
-}
