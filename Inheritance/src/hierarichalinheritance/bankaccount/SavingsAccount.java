3114f11db15010252736b72bf7b0495c383f9b97
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-07-02 21:38:27 +0530

    [Aniket] Add class eith attributes created and inherited for savings account

---
+++ Inheritance/src/hierarichalinheritance/bankaccount/SavingsAccount.java
@@ -0,0 +1,14 @@
+package hierarichalinheritance.bankaccount;
+
+public class SavingsAccount extends BankAccount {
+    double interestRate;
+
+    public SavingsAccount(String accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    void displayAccountType() {
+        System.out.println("This is a Savings Account");
+    }
+}
