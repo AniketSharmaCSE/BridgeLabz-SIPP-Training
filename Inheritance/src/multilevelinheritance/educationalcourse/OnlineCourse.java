c0e12061c418a56e4c4e3dbaff3b9fc99a9ecba4
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-07-02 21:24:10 +0530

    [Aniket] Add OnlineCourse class with inherited attributes and platform and check if recorded

---
+++ Inheritance/src/multilevelinheritance/educationalcourse/OnlineCourse.java
@@ -0,0 +1,11 @@
+package multilevelinheritance.educationalcourse;
+public class OnlineCourse extends Course {
+    String platform;
+    boolean isRecorded;
+
+    public OnlineCourse(String courseName, int duration, String platform, boolean isRecorded) {
+        super(courseName, duration);
+        this.platform = platform;
+        this.isRecorded = isRecorded;
+    }
+}
