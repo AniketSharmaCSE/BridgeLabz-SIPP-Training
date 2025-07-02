50f36c017e5bb53b4716e4d041de07203c79a35d
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-07-02 12:31:23 +0530

    [Aniket] Add Device class for device attributes

---
+++ Inheritance/src/singleinheritance/smarthomedevices/Device.java
@@ -0,0 +1,16 @@
+package singleinheritance.smarthomedevices;
+
+public class Device {
+    String deviceId;
+    String status;
+
+    public Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    public void displayStatus() {
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + status);
+    }
+}
