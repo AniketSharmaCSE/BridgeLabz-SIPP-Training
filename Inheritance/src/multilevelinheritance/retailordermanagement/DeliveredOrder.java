ad2be600d2ced3730f0c8ee35934b282d8b084bb
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-07-02 21:14:22 +0530

    [Aniket] Add delivered order class with inherited order attributes and deliveryDate

---
+++ Inheritance/src/multilevelinheritance/retailordermanagement/DeliveredOrder.java
@@ -0,0 +1,15 @@
+package multilevelinheritance.retailordermanagement;
+
+public class DeliveredOrder extends ShippedOrder {
+    String deliveryDate;
+
+    public DeliveredOrder(String orderId, String orderDate, String trackingNumber, String deliveryDate) {
+        super(orderId, orderDate, trackingNumber);
+        this.deliveryDate = deliveryDate;
+    }
+
+    @Override
+    String getOrderStatus() {
+        return "Order delivered";
+    }
+}
