fe109e0dce04829d3ce06a89eebbaec809f40144
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-07-02 21:37:09 +0530

    [Aniket] Add Bank Account class with account number and balance attributes

---
+++ Inheritance/src/hierarichalinheritance/bankaccount/BankAccount.java
@@ -0,0 +1,12 @@
+package hierarichalinheritance.bankaccount;
+
+public class BankAccount {
+    String accountNumber;
+    double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+}
+
