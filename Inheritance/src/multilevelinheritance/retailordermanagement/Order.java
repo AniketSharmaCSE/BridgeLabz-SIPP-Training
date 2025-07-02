27aa180a4a8191121e63d94d7ad4c5b7bee71cdc
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-07-02 21:11:45 +0530

    [Aniket] Add Order class with order id and date and method to display status

---
+++ Inheritance/src/multilevelinheritance/retailordermanagement/Order.java
@@ -0,0 +1,15 @@
+package multilevelinheritance.retailordermanagement;
+
+public class Order {
+    String orderId;
+    String orderDate;
+
+    public Order(String orderId, String orderDate) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+    }
+
+    String getOrderStatus() {
+        return "Order placed";
+    }
+}
