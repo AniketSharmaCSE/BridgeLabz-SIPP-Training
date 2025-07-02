491a1aaf49c2207b62dd1beaeaf64e97a3687906
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-07-02 21:22:40 +0530

    [Aniket] Add Course class with attributes name and duration

---
+++ Inheritance/src/multilevelinheritance/educationalcourse/Course.java
@@ -0,0 +1,11 @@
+package multilevelinheritance.educationalcourse;
+
+public class Course {
+    String courseName;
+    int duration; 
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+}
