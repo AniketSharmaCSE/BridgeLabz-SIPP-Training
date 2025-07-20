0cfc6476494f615e00db41249d9e3167ae187eb1
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-07-20 20:25:00 +0530

    [Aniket] Add. Bubble Sort logic separated into class for sorting student marks

---
+++ Sorting/src/sortingalgo/BubbleSort.java
@@ -0,0 +1,47 @@
+package sortingalgo;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] marks) {
+        int n = marks.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (marks[j] > marks[j + 1]) {
+                    // Swap marks[j] and marks[j + 1]
+                    int temp = marks[j];
+                    marks[j] = marks[j + 1];
+                    marks[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no elements were swapped, the array is already sorted
+            if (!swapped) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        
+        int[] marks = new int[n];
+        System.out.println("Enter the marks of " + n + " students:");
+        for (int i = 0; i < n; i++) {
+            marks[i] = sc.nextInt();
+        }
+
+        bubbleSort(marks);
+
+        System.out.println("Sorted marks in ascending order:");
+        for (int mark : marks) {
+            System.out.print(mark + " ");
+        }
+    }
+}
